496a72b37676bfd661d9e2fbfa40d30441328155
Yoshihiro Ueda <yoshihiroueda@gmail.com>
2015-05-17 23:15:58 +0900

    Answered
    PA1 answered

--- PA1_template.md
+++ PA1_template.md
@@ -1,14 +1,10 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
+# Reproducible Research: Peer Assessment 1
 
 
 ## Loading and preprocessing the data
 
-```{r echo=TRUE}
+
+```r
 activity <- read.csv("activity.csv")
 ```
 
@@ -16,34 +12,40 @@ activity <- read.csv("activity.csv")
 
 ## What is mean total number of steps taken per day?
 
-```{r echo=TRUE}
+
+```r
 steps.sum <- aggregate(steps ~ date, data = activity, FUN = sum)
 steps.sum.mean <- sprintf("%5.2f", mean(steps.sum$steps))
 steps.sum.median <- median(steps.sum$steps)
 ```
 
-Mean total steps per day: `r steps.sum.mean`
+Mean total steps per day: 10766.19
 
-Median of total steps per day: `r steps.sum.median`
+Median of total steps per day: 10765
 
 ## What is the average daily activity pattern?
 
-```{r echo=TRUE}
+
+```r
 steps.pattern <- aggregate(steps ~ interval, data = activity, FUN = mean)
 steps.max = steps.pattern$interval[ which.max(steps.pattern$steps) ]
 ```
-Interval with the max steps: `r steps.max`
+Interval with the max steps: 835
 
-```{r scatterplot}
+
+```r
 plot(x = steps.pattern$interval %/% 100 + (steps.pattern$interval %% 100)/60, 
      y = steps.pattern$steps, 
      type='l', xlab = "hours", ylab = "steps", main = "steps per 5 minutes in a day")
 ```
 
+![](PA1_template_files/figure-html/scatterplot-1.png) 
+
 
 ## Imputing missing values
 
-```{r echo=TRUE}
+
+```r
 missing <- length (activity[is.na(activity$steps), 1])
 
 activity.filled <- merge(activity, steps.pattern, by = "interval", all.x = TRUE)
@@ -57,22 +59,32 @@ colnames (activity.filled) [2] <- "steps"
 steps.filled.sum <- aggregate(steps ~ date, data = activity.filled, FUN = sum)
 steps.filled.sum.mean <- sprintf("%5.2f", mean(steps.filled.sum$steps))
 steps.filled.sum.median <- sprintf("%5.2f", median(steps.filled.sum$steps))
-
 ```
-number of missing vales: `r missing`
+number of missing vales: 2304
 
-Mean total steps per day: `r steps.filled.sum.mean` (original: `r steps.sum.mean`)
+Mean total steps per day: 10766.19 (original: 10766.19)
 
-Median of total steps per day: `r steps.filled.sum.median` (original: `r steps.sum.median`)
+Median of total steps per day: 10766.19 (original: 10765)
 
-```{r histogram}
+
+```r
 hist(steps.filled.sum$steps, xlab = "steps", ylab = "days", main = "Steps per day")
 ```
 
+![](PA1_template_files/figure-html/histogram-1.png) 
+
 ## Are there differences in activity patterns between weekdays and weekends?
 
-```{r echo=TRUE}
+
+```r
 Sys.setlocale(category="LC_ALL", "en_US")
+```
+
+```
+## [1] "en_US/en_US/en_US/C/en_US/ja_JP.UTF-8"
+```
+
+```r
 activity.filled$dayclass <- "weekday"
 activity.filled$dayclass <- ifelse ((weekdays(as.Date(activity.filled$date)) == "Sunday" |
                                        weekdays(as.Date(activity.filled$date)) == "Saturday"), 
@@ -85,7 +97,8 @@ steps.pattern.weekend <- aggregate(steps ~ interval,
                                    FUN = mean)
 ```
 
-``` {r 2pane scatterplot}
+
+```r
 par(mfrow = c(2, 1))
 plot(x = steps.pattern.weekday$interval %/% 100 + (steps.pattern.weekday$interval %% 100)/60, 
      y = steps.pattern.weekday$steps, 
@@ -94,3 +107,5 @@ plot(x = steps.pattern.weekend$interval %/% 100 + (steps.pattern.weekend$interva
      y = steps.pattern.weekend$steps, 
      type='l', xlab = "hours", ylab = "steps", main = "steps per 5 minutes in a day (weekend)")
 ```
+
+![](PA1_template_files/figure-html/2pane scatterplot-1.png) 
--- trials.R
+++ trials.R
@@ -1,77 +1,50 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
-
-
 ## Loading and preprocessing the data
 
-```{r echo=TRUE}
+setwd("~/Dropbox/Projects/DS-Reproducible/RepData_PeerAssessment1")
 activity <- read.csv("activity.csv")
-```
-
-
 
 ## What is mean total number of steps taken per day?
 
-```{r echo=TRUE}
 steps.sum <- aggregate(steps ~ date, data = activity, FUN = sum)
-steps.sum.mean <- sprintf("%5.2f", mean(steps.sum$steps))
+steps.sum.mean <- mean(steps.sum$steps)
 steps.sum.median <- median(steps.sum$steps)
-```
-
-Mean total steps per day: `r steps.sum.mean`
-
-Median of total steps per day: `r steps.sum.median`
 
 ## What is the average daily activity pattern?
 
-```{r echo=TRUE}
 steps.pattern <- aggregate(steps ~ interval, data = activity, FUN = mean)
+plot(steps.pattern$interval %/% 100 + (steps.pattern$interval %% 100)/60, steps.pattern$steps, type='l')
+
 steps.max = steps.pattern$interval[ which.max(steps.pattern$steps) ]
-```
-Interval with the max steps: `r steps.max`
-
-```{r scatterplot}
-plot(x = steps.pattern$interval %/% 100 + (steps.pattern$interval %% 100)/60, 
-     y = steps.pattern$steps, 
-     type='l', xlab = "hours", ylab = "steps", main = "steps per 5 minutes in a day")
-```
-
 
 ## Imputing missing values
 
-```{r echo=TRUE}
+# 1. Calculate and report the total number of missing values in th
+
 missing <- length (activity[is.na(activity$steps), 1])
 
+# 2. Devise a strategy for filling in all of the missing values in the dataset. 
+# -> Use mean of the inverval
+
+# 3. Create a new dataset that is equal to the original dataset but with the missing data filled in.
+
 activity.filled <- merge(activity, steps.pattern, by = "interval", all.x = TRUE)
+# x activity.filled <- apply(activity.filled, 1, function (x){if (is.na(x[2])) x[2] <- x[4] })
 for (i in 1:dim(activity.filled)[1]) {
   if (is.na(activity.filled$steps.x[i])) activity.filled$steps.x[i] <- activity.filled$steps.y[i]
 }
 activity.filled <- activity.filled[ order(activity.filled$date, activity.filled$interval), c(1:3)]
 colnames (activity.filled) [2] <- "steps"
 
+# 4. Make a histogram of the total number of steps taken each day and Calculate and report the mean and median total number of steps taken per day. Do these values differ from the estimates from the first part of the assignment? What is the impact of imputing missing data on the estimates of the total daily number of steps?
 
 steps.filled.sum <- aggregate(steps ~ date, data = activity.filled, FUN = sum)
-steps.filled.sum.mean <- sprintf("%5.2f", mean(steps.filled.sum$steps))
-steps.filled.sum.median <- sprintf("%5.2f", median(steps.filled.sum$steps))
+steps.filled.sum.mean <- mean(steps.filled.sum$steps)
+steps.filled.sum.median <- median(steps.filled.sum$steps)
 
-```
-number of missing vales: `r missing`
-
-Mean total steps per day: `r steps.filled.sum.mean` (original: `r steps.sum.mean`)
-
-Median of total steps per day: `r steps.filled.sum.median` (original: `r steps.sum.median`)
-
-```{r histogram}
 hist(steps.filled.sum$steps, xlab = "steps", ylab = "days", main = "Steps per day")
-```
 
 ## Are there differences in activity patterns between weekdays and weekends?
 
-```{r echo=TRUE}
 Sys.setlocale(category="LC_ALL", "en_US")
 activity.filled$dayclass <- "weekday"
 activity.filled$dayclass <- ifelse ((weekdays(as.Date(activity.filled$date)) == "Sunday" |
@@ -83,9 +56,6 @@ steps.pattern.weekday <- aggregate(steps ~ interval,
 steps.pattern.weekend <- aggregate(steps ~ interval, 
                                    data = activity.filled[activity.filled$dayclass == "weekend",], 
                                    FUN = mean)
-```
-
-``` {r 2pane scatterplot}
 par(mfrow = c(2, 1))
 plot(x = steps.pattern.weekday$interval %/% 100 + (steps.pattern.weekday$interval %% 100)/60, 
      y = steps.pattern.weekday$steps, 
@@ -93,4 +63,7 @@ plot(x = steps.pattern.weekday$interval %/% 100 + (steps.pattern.weekday$interva
 plot(x = steps.pattern.weekend$interval %/% 100 + (steps.pattern.weekend$interval %% 100)/60, 
      y = steps.pattern.weekend$steps, 
      type='l', xlab = "hours", ylab = "steps", main = "steps per 5 minutes in a day (weekend)")
-```
+
+
+
+
